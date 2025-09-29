c5a749d1f12561580b968dfb35c972e0bcbd07e2
Thomas Llamzon <tllamzon@uwo.ca>
2025-09-29 10:15:53 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/tl-portfolio/",
   plugins: [react(), tailwindcss()],
+  base: '/',
   resolve: {
     alias: {
       '@': '/src',
